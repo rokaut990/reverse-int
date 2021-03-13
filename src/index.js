d5d17e1249f61aa5e2d7e255540c8bdda77e5f12
rokaut990 <64099437+rokaut990@users.noreply.github.com>
2021-03-13 21:17:58 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const { strikethrough } = require("colors");
+
 module.exports = function reverse (n) {
-  
+    if (n < 0){
+        n = n * (-1);
+    }  
+    str = n + '';
+    str = str.split('').reverse().join('');
+    return str;
 }
